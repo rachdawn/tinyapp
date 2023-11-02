650adf3286b2853bcf91ff37e07dba3783ad4466
Rachel Little <racheldawnlittle@gmail.com>
2023-11-01 20:18:49 -0600

    added functionality to produce short id and redirect to the coinciding longURL

--- express_server.js
+++ express_server.js
@@ -4,23 +4,24 @@ const PORT = 8080; // default port 8080
 
 
 //implement a function that generates a random 6-digit alphanumeric string
-function generateRandomString(length) {
+function generateRandomString() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < 6; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         result += characters.charAt(randomIndex);
     }
     return result;
 };
-console.log(generateRandomString(6));
+console.log(generateRandomString());
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+  
 };
 
 app.get("/", (req, res) => {
@@ -44,21 +45,30 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  // get long url from req.body.longURL
+  const longURL = req.body.longURL;
+  //generate short id
+  const id = generateRandomString();
+  //store in database
+  urlDatabase[id] = longURL;
+  //redirect
+  res.redirect(`/urls/${id}`);
+});
+
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
